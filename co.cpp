348d8efe960572cc253994880ce3a27ac29c6c9e
Suman Gole <60792767+golesuman@users.noreply.github.com>
2020-10-09 15:32:16 +0545

    Add files via upload

---
+++ co.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+    public:
+      complex()
+         {
+        cout<<"Hello World!"<<endl;
+    }
+};
+int main()
+{
+    complex c1,c2;
+}
